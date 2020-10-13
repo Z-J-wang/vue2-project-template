6ca0d6f229ee0491a0791ce64cba7bc3ce0f7534
Jay <zhi-jie.wang@mail.foxconn.com>
2020-10-13 09:47:25 +0800

    feat: 调整axios->module 的命名

--- src/axios/HttpApi.js
+++ src/axios/HttpApi.js
@@ -1,8 +1,8 @@
 // httpApi 为 httpApi 汇总类，以及通用的 httpApi
 
 import mix from '../util/mixin'
-import LoginApi from './module/LoginApi'
-import commonApi from './module/UserApi'
+import LoginApi from './modules/LoginApi'
+import commonApi from './modules/UserApi'
 
 class httpApi extends mix(LoginApi, commonApi) {
 }
