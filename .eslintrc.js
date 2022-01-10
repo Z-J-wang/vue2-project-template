acfc399983f5c879a282456e208d7d027c3e8875
Jay <zhi-jie.wang@mail.foxconn.com>
2022-01-10 18:04:29 +0800

    build: 配置调整

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,55 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+
   parserOptions: {
     parser: 'babel-eslint'
   },
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'space-before-function-paren': "off",
-    'indent': 4
-  }
-}
+    'vue/html-indent': [
+      'error',
+      2,
+      {
+        attribute: 1,
+        alignAttributesVertically: true,
+        ignores: []
+      }
+    ],
+    'vue/max-attributes-per-line': [
+      2,
+      {
+        singleline: 10,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ],
+    'vue/name-property-casing': ['error', 'kebab-case'],
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'no-spaced-func': 2,
+    'no-const-assign': 2,
+    'no-alert': 0,
+    'space-before-function-paren': [0, 'always'],
+    'no-useless-escape': 0,
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: [1, 'single'],
+    semi: [2, 'always']
+  },
+
+  extends: ['plugin:vue/essential', 'eslint:recommended'],
+
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true
+      }
+    }
+  ]
+};
--- vue.config.js
+++ vue.config.js
@@ -1,147 +1,184 @@
 // 本文件的详细说明请查阅：https://blog.csdn.net/weixin_44869002/article/details/106826503
 
-const path = require('path')
-// const UglifyPlugin = require('uglifyjs-webpack-plugin')
-// const CompressionPlugin = require("compression-webpack-plugin")
-// const webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin'); // js压缩插件
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-    publicPath: './', // 基本路径
-    outputDir: 'dist', // 输出文件目录
-    filenameHashing: true, // 生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存
-    lintOnSave: false, // eslint-loader 是否在保存的时候检查
-    productionSourceMap: true, // 生产环境是否生成 sourceMap 文件
+  publicPath: '/', // 基本路径
+  assetsDir: 'assets',
+  outputDir: 'dist', // 输出文件目录
+  filenameHashing: true, // 生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存
+  productionSourceMap: false, // 生产环境是否生成 sourceMap 文件
 
-    chainWebpack: config => {
-        // 开启图片压缩
-        config.module.rule('images')
-            .test(/\.(png|jpe?g|gif|svg)(\?.*)?$/)
-            .use('url-loader')
-            .loader('url-loader')
-            .options({
-                limit: 10240 // 图片小于10k转为base64,默认4k
-            })
-            .end()
-            .test(/\.(png|jpe?g|gif|svg)(\?.*)?$/)
-            .use('image-webpack-loader')
-            .loader('image-webpack-loader')
-            .options({ bypassOnDebug: true })
-            .end()
+  chainWebpack: config => {
+    // 启用打包分析工具 BundleAnalyzer
+    config.when(process.env.NODE_ENV_BUNDLE_ANALYZE === 'true', config => {
+      config.plugin('BundleAnalyzerPlugin').use(BundleAnalyzerPlugin);
+    });
 
-        // 配置Jquery
-        // config.plugin('provide').use(webpack.ProvidePlugin, [{
-        //   $: 'jquery',
-        //   jQuery: 'jquery',
-        //   'window.jQuery': 'jquery',
-        //   Popper: ['popper.js', 'default']
-        // }])
+    // 开启图片压缩
+    config.module
+      .rule('images')
+      .test(/\.(png|jpe?g|gif)(\?.*)?$/)
+      .use('url-loader')
+      .loader('url-loader')
+      .tap(options => {
+        options.limit = 10240; // 小于10kb的图片都转为base64
+        return options;
+      })
+      .end()
+      // 启用图片压缩功能
+      .use('image-webpack-loader')
+      .loader('image-webpack-loader')
+      .options({ bypassOnDebug: false })
+      .end();
 
-        // 开启js、css 文件的 gzip 压缩。
-        // 注意，gzip 压缩需要后端配合使用。如果后端没有开发 gzip 。请关闭功能。
-        // if (process.env.NODE_ENV === 'production') {
-        //   config.plugin('compressionPlugin')
-        //   .use(new CompressionPlugin({
-        //     test:/\.js$|\.html$|.\css/, // 匹配文件名
-        //     threshold: 10240, // 对超过10k的数据压缩
-        //     deleteOriginalAssets: false // 不删除源文件
-        //   }))
-        // }
-    },
+    /**
+     * 将svg图片转为组件使用
+     */
+    // const svgRule = config.module.rule('svg');
+    // svgRule.uses.clear();
+    // svgRule.use('vue-svg-loader').loader('vue-svg-loader');
 
-    configureWebpack: (config) => {
-        if (process.env.NODE_ENV === 'production') {
-            // 为生产环境修改配置...
-            config.mode = 'production'
+    // 配置Jquery
+    config.plugin('provide').use(webpack.ProvidePlugin, [
+      {
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        Popper: ['popper.js', 'default']
+      }
+    ]);
+  },
 
-            // 将每个依赖包打包成单独的js文件
-            const optimization = {
-                runtimeChunk: 'single',
-                splitChunks: {
-                    chunks: 'all',
-                    maxInitialRequests: Infinity,
-                    minSize: 20000, // 依赖包超过20000bit将被单独打包
-                    cacheGroups: {
-                        vendor: {
-                            test: /[\\/]node_modules[\\/]/,
-                            name (module) {
-                                // get the name. E.g. node_modules/packageName/not/this/part.js
-                                // or node_modules/packageName
-                                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
-                                // npm package names are URL-safe, but some servers don't like @ symbols
-                                return `npm.${packageName.replace('@', '')}`
-                            }
-                        }
-                    }
-                }
-
-                // // 移除console
-                // minimizer: [new UglifyPlugin({
-                //   uglifyOptions: {
-                //     warnings: false,
-                //     compress: {
-                //       drop_console: true, // console
-                //       drop_debugger: false,
-                //       pure_funcs: ['console.log'] // 移除console
-                //     }
-                //   }
-                // })]
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // 为生产环境修改配置...
+      config.mode = 'production';
+      // 将每个依赖包打包成单独的js文件
+      const optimization = {
+        runtimeChunk: 'single',
+        splitChunks: {
+          chunks: 'all', // 选择哪些 chunk 进行优化, 有效值 all，async 和 initial
+          maxAsyncRequests: 30, // 按需加载时的最大并行请求数
+          maxInitialRequests: 30, // 入口点的最大并行请求数
+          minSize: 30000, // 生成 chunk 的最小体积（以 bytes 为单位）
+          maxSize: 1024000, // 生成 chunk 的最大体积（以 bytes 为单位），对大于该值的js尝试做拆分
+          cacheGroups: {
+            vendors: {
+              test: /[\\/]node_modules[\\/]/,
+              priority: -10,
+              chunks: 'all',
+              name(module) {
+                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+                return `npm.${packageName.replace('@', '')}`;
+              }
+            },
+            common: {
+              name: `chunk-common`,
+              minChunks: 2,
+              priority: -20,
+              chunks: 'all',
+              reuseExistingChunk: true
+            },
+            element: {
+              chunks: 'all',
+              name: `element-ui`,
+              test: /[\\/]element-ui[\\/]/,
+              minChunks: 2,
+              priority: 0
+            },
+            merges: {
+              test: /[\\/]supplyAndDemand[\\/]|[\\/]src[\\/]src\/views[\\/]/,
+              priority: 0,
+              name: 'merges'
             }
-            Object.assign(config, {
-                optimization
-            })
-        } else {
-            // 为开发环境修改配置...
-            config.mode = 'development'
+          }
+        },
+        // 插件优化
+        minimize: true,
+        minimizer: [
+          // js压缩插件
+          new TerserPlugin({
+            parallel: true,
+            // sourceMap: true,
+            terserOptions: {
+              extractComments: false, // 注释剥离功能
+              format: {
+                comments: false // 删除所有注释
+              },
+              warnings: true,
+              // parse: {},
+              compress: {
+                drop_console: true,
+                drop_debugger: false,
+                pure_funcs: ['console.log'] // 移除console
+              }
+            }
+          })
+        ]
+      };
+      Object.assign(config, {
+        optimization
+      });
+    } else {
+      // 为开发环境修改配置...
+      config.mode = 'development';
+    }
+    Object.assign(config, {
+      // 开发生产共同配置
+      resolve: {
+        alias: {
+          // 别名配置
+          '@': path.resolve(__dirname, './src'),
+          '@a': path.resolve(__dirname, './src/assets'),
+          '@c': path.resolve(__dirname, './src/components')
         }
-        Object.assign(config, {
-            // 开发生产共同配置
-            resolve: {
-                alias: {
-                    '@': path.resolve(__dirname, './src'),
-                    '@a': path.resolve(__dirname, './src/assets'),
-                    '@c': path.resolve(__dirname, './src/components'),
-                    '@v': path.resolve(__dirname, './src/views')
-                } // 别名配置
-            }
-        })
-    },
+      }
+    });
+  },
 
-    // css相关配置
-    css: {
-        extract: true, // 是否使用css分离插件 ExtractTextPlugin
-        sourceMap: true, // 开启 CSS source maps?
-        requireModuleExtension: true, // 是否仅对文件名包含module的css相关文件使用 CSS Modules
-        loaderOptions: {
-            css: {
-                modules: {
-                    localIdentName: '[local]_[hash:base64:8]' // 设定 CSS Modules 命名规则
-                }
-            }
-        } // css预设器配置项 详见https://cli.vuejs.org/zh/config/#css-loaderoptions
-    },
-    parallel: require('os').cpus().length > 1, // 是否为 Babel 或 TypeScript 使用 thread-loader。该选项在系统的 CPU 有多于一个内核时自动启用，仅作用于生产构建。
-    // webpack-dev-server 相关配置
-    devServer: {
-        open: true,
-        // inline: true, // 开启实时刷新
-        // host: '0.0.0.0', // 允许外部ip访问
-        port: 8024, // 端口
-        https: false, // 启用https
-        overlay: {
-            warnings: true,
-            errors: true
-        } // 错误、警告在页面弹出
-        // proxy: {
-        //   '/api': {
-        //     target: '',
-        //     changeOrigin: true, // 允许websockets跨域
-        //     ws: true,
-        //     pathRewrite: {
-        //       '^/api': ''
-        //     }
-        //   }
-        // } // 代理转发配置，用于调试环境
-    },
-    // 第三方插件配置
-    pluginOptions: {}
-}
+  // css相关配置
+  css: {
+    extract: false, // 是否使用css分离插件 ExtractTextPlugin
+    sourceMap: true, // 开启 CSS source maps?
+    requireModuleExtension: true, // 是否仅对文件名包含module的css相关文件使用 CSS Modules
+    loaderOptions: {
+      css: {
+        modules: {
+          localIdentName: '[local]_[hash:base64:8]' // 设定 CSS Modules 命名规则
+        }
+      }
+    } // css预设器配置项 详见https://cli.vuejs.org/zh/config/#css-loaderoptions
+  },
+
+  // 是否为 Babel 或 TypeScript 使用 thread-loader。该选项在系统的 CPU 有多于一个内核时自动启用，仅作用于生产构建。
+  parallel: require('os').cpus().length > 1,
+
+  // webpack-dev-server 相关配置
+  devServer: {
+    // proxy: {
+    //   '/api': {
+    //     target: '',
+    //     changeOrigin: true // 允许跨域
+    //     // pathRewrite: { '^/api': '' }
+    //   }
+    // },
+    open: false,
+    inline: true, // 开启实时刷新
+    // host: '0.0.0.0', // 允许外部ip访问
+    port: 8024, // 端口
+    https: false, // 启用https
+    overlay: {
+      warnings: true,
+      errors: true
+    } // 错误、警告在页面弹出
+  },
+
+  // 第三方插件配置
+  pluginOptions: {},
+
+  lintOnSave: false
+};
