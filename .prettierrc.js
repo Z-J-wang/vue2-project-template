2a6f7e2e0de94dabac5ae24b7c849959b44d9adf
wangzhijie <135653671@qq.com>
2023-05-21 00:09:42 +0800

    .prettierrc 更新

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  printWidth: 120, // 一行的字符数，如果超过会进行换行，默认为80
-  tabWidth: 2,  // 一个tab代表几个空格数，默认为2
+  printWidth: 120, // 一行的字符数，如果超过会进行换行，默认为80。注意，该值必须设置为120，否则与eslint规则冲突
+  tabWidth: 2, // 一个tab代表几个空格数，默认为2
   useTabs: false, // 是否使用tab进行缩进，默认为false，表示用空格进行缩减
   singleQuote: true, // 字符串是否使用单引号，默认为false，使用双引号
   semi: true, // 行位是否使用分号，默认为true
